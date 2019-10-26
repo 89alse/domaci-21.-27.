781bc166cc009702e3ca8d7ba96d31e27777a2aa
ayoushab <56487764+ayoushab@users.noreply.github.com>
2019-10-26 15:25:26 +0200

    Add files via upload

---
+++ domaci2/domaci2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+            int a = 0, b = 1, c = 0;
+            int unos = int.Parse(Console.ReadLine());
+
+
+
+
+            for (int i = 0; i < unos; i++)
+            {
+                Console.WriteLine(c);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
