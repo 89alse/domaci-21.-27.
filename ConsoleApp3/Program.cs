a205907f640046b3714444622ff4b18b327a4b2d
Zen Pro <Zen Pro@LAPTOP-VDHQGOSD>
2019-10-26 14:45:37 +0200

    domaci

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci_za_21._27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+            int Broj1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Broj1; i++)
+            {
+
+                if (i % 15 == 0 && i != 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 5 == 0 && i != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0 && i != 0)
+                 {
+                    Console.WriteLine("Fizz");
+                 }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+       
+
+
+        
+    }
+}
